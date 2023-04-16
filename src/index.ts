5c3461db5bea08bf896e2fb31d0fe8f3d44df9cd
Julio Levi <jlevicarvalho@gmail.com>
2023-04-15 21:02:20 -0300

    refact: moved to vite

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,63 @@
-import { app, Tray } from "electron";
-
+import { app, Menu } from "electron";
 import { createTrayMenu } from "./tray/createMenu";
 import { createTray } from "./tray/createTray";
 
-import reloader from "electron-reloader";
+// try {
+//   require("electron-reloader")(module);
+// } catch (_) {}
 
-reloader(module, {
-  debug: false,
-  watchRenderer: true,
-});
+const data: any[] = [];
+(async () => {
+  await app.whenReady();
+  const tray = createTray();
+  const ini = Menu.buildFromTemplate([
+    { id: "1", label: "Item1", type: "normal", role: "viewMenu" },
+    // { label: "Item2", type: "submenu" },
+    // { label: "Item3", type: "normal" },
+    // { label: "Item4", type: "radio" },
+    // { label: "Item4", type: "separator" },
+  ]);
 
-let tray: Tray;
+  const initial = Menu.setApplicationMenu(ini);
+  tray.setContextMenu(ini);
+  const menu = Menu.getApplicationMenu();
+  menu?.on("menu-will-show", () => {
+    console.log("show");
+  });
+  menu!.addListener("menu-will-show", () => console.log("will-show"));
+  menu!.addListener("menu-will-close", () => console.log("will-show"));
+  tray.on("click", function () {
+    console.log(menu?.items.map((i) => i.id));
+    menu?.popup();
+    tray.popUpContextMenu();
+    tray.displayBalloon({ content: "123", title: "312" });
+  });
+})();
 
-app.on("ready", async () => {
-  tray = createTray();
-  await createTrayMenu(tray);
-});
+// app.on("ready", async () => {
+//   const tray = createTray();
+//   const onRightClick = () => {
+//     console.log("right-click");
+//     tray.setIgnoreDoubleClickEvents(false);
+//   };
+//   // console.log(tray);
+//   const initial = Menu.buildFromTemplate([
+//     { label: "Item1", type: "checkbox" },
+//     // { label: "Item2", type: "submenu" },
+//     // { label: "Item3", type: "normal" },
+//     // { label: "Item4", type: "radio" },
+//     // { label: "Item4", type: "separator" },
+//   ]);
+//   tray.on("click", function () {
+//     tray.popUpContextMenu();
+//     tray.displayBalloon({ content: "123", title: "312" });
+//   });
+//   tray.setToolTip("This is my application.");
+//   tray.setContextMenu(initial);
+//   // tray.addListener("click", () => console.log("click"));
+//   tray.addListener("right-click", () => onRightClick());
+
+//   // tray.once('')
+
+//   // await createTrayMenu(tray);
+// });
--- src/tray/createTray.ts
+++ src/tray/createTray.ts
@@ -1,8 +1,13 @@
 import { Tray, nativeImage } from "electron";
 import path from "path";
 
+const config =
+  process.env.NODE_ENV === "development"
+    ? path.join(__dirname, "..")
+    : path.join(__dirname);
+
 export const defaultIcon = nativeImage.createFromPath(
-  path.resolve(__dirname, "..", "assets", "icon.png")
+  path.resolve(config, "assets", "icon.png")
 );
 
 export function createTray() {
